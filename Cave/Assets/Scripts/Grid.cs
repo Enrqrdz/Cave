2c4a75a6346143f2b6d885e0de3655cb7f70f2d7
BryceKopan <BryceKopan@gmail.com>
2019-11-19 00:27:51 -0600

    Grid Initial Commit

---
+++ Cave/Assets/Scripts/Grid.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Grid : MonoBehaviour
+{
+	[SerializeField]
+	private float size = 1f;
+
+	[SerializeField]
+	private float height = .5f;
+
+	public void SnapObjectToGrid(GameObject target)
+	{
+		target.transform.position = GetNearestPointOnGrid(target.transform.position);
+	}
+
+	public Vector3 GetNearestPointOnGrid(Vector3 position)
+	{
+		position -= transform.position;
+
+		int xCount = Mathf.RoundToInt(position.x / size);
+		int yCount = Mathf.RoundToInt(position.y / height);
+		int zCount = Mathf.RoundToInt(position.z / size);
+
+		Vector3 result = new Vector3(xCount * size, yCount * height, zCount * size);
+		result += transform.position;
+		return result;
+	}
+}
+
---
+++ Cave/Assets/Scripts/SnapToGrid.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SnapToGrid : MonoBehaviour
+{
+	Grid grid;
+
+	void Start()
+	{
+		grid = GameObject.FindGameObjectWithTag("Grid").GetComponent<Grid>();
+		grid.SnapObjectToGrid(gameObject);
+	}
+
+	void Update()
+	{
+		if (transform.hasChanged)
+		{
+			grid.SnapObjectToGrid(gameObject);
+		}
+	}
+}
